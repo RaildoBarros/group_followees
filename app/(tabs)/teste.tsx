facd13851bcb4e39b6ac651a51528a7be912bc96
raildobarros <raildo.barros@gmail.com>
2024-05-16 22:58:54 -0400

    Testes com bottom sheet

--- app/(tabs)/teste.tsx
+++ app/(tabs)/teste.tsx
@@ -1,92 +1,50 @@
-import React, { useEffect, useState } from 'react';
-import { StyleSheet } from 'react-native';
-import { Dropdown } from 'react-native-element-dropdown';
-import AntDesign from '@expo/vector-icons/AntDesign';
-import { getGroups } from '@/services/api.service';
+import React, { useCallback, useRef, useMemo } from "react";
+import { StyleSheet, View, Text, Button } from "react-native";
+import BottomSheet, { BottomSheetView } from "@gorhom/.bottom-sheet-1WgOr4H9";
 
-const data = [
-    { name: 'Item 1', value: '1' },
-    { name: 'Item 2', value: '2' },
-    { name: 'Item 3', value: '3' },
-    { name: 'Item 4', value: '4' },
-    { name: 'Item 5', value: '5' },
-    { name: 'Item 6', value: '6' },
-    { name: 'Item 7', value: '7' },
-    { name: 'Item 8', value: '8' },
-];
+const App = () => {
+  // hooks
+  const sheetRef = useRef<BottomSheet>(null);
 
+  // variables
+  const snapPoints = useMemo(() => ["25%", "50%", "90%"], []);
 
-const DropdownComponent = () => {
-    const [value, setValue] = useState(null);
+  // callbacks
+  const handleSheetChange = useCallback((index) => {
+    console.log("handleSheetChange", index);
+  }, []);
+  const handleSnapPress = useCallback((index) => {
+    sheetRef.current?.snapToIndex(index);
+  }, []);
+  const handleClosePress = useCallback(() => {
+    sheetRef.current?.close();
+  }, []);
 
-    const [groups, setGroups] = useState([]);
-
-    const loadGroups = async () => {
-        try {
-            const groups = await getGroups();
-            console.log(groups);
-            setGroups(groups);
-        } catch (error) {
-            console.error('Erro ao pegar os grupos:', error);
-        } finally {
-            console.log("Busca finalizada.")
-        }
-    };
-
-    useEffect(() => {
-        loadGroups();
-    }, []);
-
-
-    return (
-        <Dropdown
-            style={styles.dropdown}
-            placeholderStyle={styles.placeholderStyle}
-            selectedTextStyle={styles.selectedTextStyle}
-            inputSearchStyle={styles.inputSearchStyle}
-            iconStyle={styles.iconStyle}
-            data={groups}
-            search
-            maxHeight={300}
-            labelField="name"
-            valueField="id"
-            placeholder="Select item"
-            searchPlaceholder="Search..."
-            value={value}
-            onChange={item => {
-                setValue(item.id);
-            }}
-            renderLeftIcon={() => (
-                <AntDesign style={styles.icon} color="black" name="Safety" size={20} />
-            )}
-        />
-    );
+  // render
+  return (
+    <View style={styles.container}>
+      <Button title="Snap To 90%" onPress={() => handleSnapPress(2)} />
+      <Button title="Snap To 50%" onPress={() => handleSnapPress(1)} />
+      <Button title="Snap To 25%" onPress={() => handleSnapPress(0)} />
+      <Button title="Close" onPress={() => handleClosePress()} />
+      <BottomSheet
+        ref={sheetRef}
+        snapPoints={snapPoints}
+        onChange={handleSheetChange}
+      >
+        <BottomSheetView>
+          <Text>Awesome 🔥</Text>
+        </BottomSheetView>
+      </BottomSheet>
+    </View>
+  );
 };
 
-export default DropdownComponent;
-
 const styles = StyleSheet.create({
-    dropdown: {
-        margin: 16,
-        height: 50,
-        borderBottomColor: 'gray',
-        borderBottomWidth: 0.5,
-    },
-    icon: {
-        marginRight: 5,
-    },
-    placeholderStyle: {
-        fontSize: 16,
-    },
-    selectedTextStyle: {
-        fontSize: 16,
-    },
-    iconStyle: {
-        width: 20,
-        height: 20,
-    },
-    inputSearchStyle: {
-        height: 40,
-        fontSize: 16,
-    },
+  container: {
+    flex: 1,
+    paddingTop: 200,
+  },
 });
+
+export default App;
